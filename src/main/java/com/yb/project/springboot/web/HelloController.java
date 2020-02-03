b3aa16108c9bb9d723632cb4e1f5b4878772ac6e
yb <syb4819@naver.com>
2020-02-03 17:51:35 +0900

    gitignore 등록

---
+++ src/main/java/com/yb/project/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.yb.project.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+}
